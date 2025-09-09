2f7c1a504cf9d8ba0ae8d61e3c7b0bc3ed190233
harshithasavadatti <harshitha.savadatti@gmail.com>
2025-09-09 16:27:15 +0530

    Update 4_Financial Health & Affordability.py

--- pages/4_Financial Health & Affordability.py
+++ pages/4_Financial Health & Affordability.py
@@ -2,8 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import altair as alt
-import matplotlib.pyplot as plt
-import seaborn as sns   
+  
 from utils.load_data import load_data
 
 # Load data
