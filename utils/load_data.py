d443968752c00481cb42acd42888b0545e32399b
harshithasavadatti <harshitha.savadatti@gmail.com>
2025-09-09 16:07:32 +0530

    Update load_data.py

--- utils/load_data.py
+++ utils/load_data.py
@@ -3,6 +3,6 @@ import pandas as pd
 import streamlit as st
 
 @st.cache_data
-def load_data(file_path="H:/Home Dashboard/preprocessing/reduced_dataset.csv"):
+def load_data(file_path="preprocessing/reduced_dataset.csv"):
     df = pd.read_csv(file_path)
     return df
